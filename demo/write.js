cb067c4606115aaf831c73df7abf7f5279a8a1a9
yaohui <yaohui@qunhemail.com>
2021-10-12 21:20:43 +0800

    learn code

--- demo/write.js
+++ demo/write.js
@@ -13,3 +13,30 @@ const myInstanceOf = (target, origin) => {
   }
   return false;
 };
+
+/**
+ * new实现
+ */
+function objectFactory(){
+  
+  // 取构造函数
+  const constructor = [].shift.call(arguments)
+  if(typeof constructor !== 'function'){
+    throw new Error('constructor must be a function')
+  }
+  // Object.setPrototypeOf(obj, Constructor.prototype)来设置原型对象
+  const obj = Object.create(constructor.prototype)
+  const res = constructor.apply(obj, arguments)
+  console.log(res, obj)
+  // obj.__proto__ = constructor.prototype
+
+}
+
+function Student(name, age){
+  this.name = name;
+  this.age = age;
+  // this.doSth();
+  // return Error();
+}
+
+objectFactory(Student, 'd', 18)
