b95cc6c63dc7153794a7e08500accebe3ee7fe62
yaohui <yaohui@qunhemail.com>
2021-10-13 21:28:35 +0800

    learn code

--- demo/write.js
+++ demo/write.js
@@ -18,7 +18,6 @@ const myInstanceOf = (target, origin) => {
  * new实现
  */
 function objectFactory(){
-  
   // 取构造函数
   const constructor = [].shift.call(arguments)
   if(typeof constructor !== 'function'){
@@ -27,16 +26,25 @@ function objectFactory(){
   // Object.setPrototypeOf(obj, Constructor.prototype)来设置原型对象
   const obj = Object.create(constructor.prototype)
   const res = constructor.apply(obj, arguments)
-  console.log(res, obj)
-  // obj.__proto__ = constructor.prototype
-
+  return typeof res === 'object' ? res : obj
 }
 
 function Student(name, age){
   this.name = name;
   this.age = age;
-  // this.doSth();
-  // return Error();
 }
 
 objectFactory(Student, 'd', 18)
+
+/**
+ * 数组扁平化
+ * 把多维数组转化成一维数组
+ */
+const arr1 = [1, [2, 3, [2]]]
+arr1.flat() // [1, 2, 3, Array(2)]
+arr1.flat(2) // [1, 2, 3, 2]
+// 需要知道扁平化的数组是具体维度才可以
+// 可以使用 Infinity
+arr1.flat(Infinity) // 无需知道数组的维度
+
+
